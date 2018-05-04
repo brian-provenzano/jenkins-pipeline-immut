842a475ae3bb47d9d5ed899acc188a05d72db149
Brian Provenzano <bproven@gmail.com>
2018-05-04 12:17:44 -0700

    fix: incorrect ref in cleanup ami script

--- cleanup_ami.sh
+++ cleanup_ami.sh
@@ -8,11 +8,11 @@ export AWS_SECRET_ACCESS_KEY=$2
 export AWS_DEFAULT_REGION=$3
 #Find snapshots associated with AMI.
 aws ec2 describe-images --image-ids `cat ami.txt` | grep snap | awk ' { print $4 }' > snapami.txt
-echo -e "Following are the snapshots associated with ami : `cat snap.txt`:\n "
+echo -e "Following are the snapshots associated with ami : `cat snapami.txt`:\n "
 echo -e "Deregister the AMI... \n"
 aws ec2 deregister-image --image-id `cat ami.txt`
 echo -e "\nDeleting the associated snapshots.... \n"
-for i in `cat snap.txt`; do aws ec2 delete-snapshot --snapshot-id $i; done
+for i in `cat snapami.txt`; do aws ec2 delete-snapshot --snapshot-id $i; done
 #echo -e "\nDelete the snapshot holder text file..."
 #rm -f snapami.txt
 
