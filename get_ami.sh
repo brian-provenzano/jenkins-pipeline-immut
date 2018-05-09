f8a58e1b31083bf82576ddf82ca96c91c15722fc
Brian Provenzano <bproven@gmail.com>
2018-05-09 12:22:34 -0700

    fix: bath path on ami.txt

--- get_ami.sh
+++ get_ami.sh
@@ -2,4 +2,4 @@
 export NEW_AMI=$(jq -r '.builds[].artifact_id' manifest.json | cut -d':' -f2)
 echo "From Packer post processor - the AMI created: ${NEW_AMI}"
 echo "Capturing AMI in ami.txt locally..."
-echo -n $NEW_AMI > ami.txt
+echo -n $NEW_AMI > ../ami.txt
